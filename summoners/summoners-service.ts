b311e7dbdf5588eca6494e9c95bec0a6e691018f
SWarrior97 <32170183+SWarrior97@users.noreply.github.com>
2020-11-01 17:38:50 +0000

    Update summoners-service.ts

--- summoners/summoners-service.ts
+++ summoners/summoners-service.ts
@@ -33,16 +33,89 @@ function getRegion(serverNumber: number) {
 }
 
 async function getSummoners(serverNumber: number, name:string) {
+    console.info("qweqweqweeqwqweqweqwe");
     const server = getServer(serverNumber);
     const region = getRegion(serverNumber);
     try{
+        console.log("ashjgvdasghvdhgvasdgvhasgvdvasgd");
         let rift = await axios.get('https://' + server + '/lol/summoner/v4/summoners/by-name/' + name + '?api_key=' + key);
         let tft = await axios.get('https://' + server + '/tft/summoner/v1/summoners/by-name/' + name + '?api_key=' + key);
         let lor = null;
         if((rift && rift.data && rift.data.puuid) || (tft && tft.data && tft.data.puuid)) {
-            let riftRankeds = await axios.get('https://' + server + '/lol/league/v4/entries/by-summoner/' + rift.data.id + '?api_key=' + key);
-            rift.data.rankeds = riftRankeds.data;
             lor = await axios.get('https://' + region + '/riot/account/v1/accounts/by-puuid/' + (rift.data.puuid ? rift.data.puuid : tft.data.puuid) + '?api_key=' + key);
+            let riftRankeds = await axios.get('https://' + server + '/lol/league/v4/entries/by-summoner/' + rift.data.id + '?api_key=' + key);
+            rift.data.rankeds = riftRankeds.data ? riftRankeds.data : [];
+            
+            let games = await axios.get('https://' + server + '/lol/match/v4/matchlists/by-account/' + rift.data.accountId + '?api_key=' + key);
+
+            if(games.data){
+                let auxGames:any[] = [];
+                games.data.matches.forEach(function(record:any){
+                    auxGames.push({gameId:record.gameId,champion:record.champion,queue:record.queue});
+                });
+
+                let filterGames = auxGames.slice(0,10);
+                let gamesWithDetails:any[] = [];
+
+                filterGames.forEach(async function(record:any){
+                    let gamedetails = await axios.get('https://' + server + '/lol/match/v4/matches/' + record.gameId + '?api_key=' + key);
+                    let userParticipantId = 0;
+                    if(gamedetails.data){
+                        let time = gamedetails.data.gameDuration;
+                        let minutes = Math.floor(time / 60);
+                        let seconds = time - minutes * 60;
+                        let duration = minutes + ':'+ seconds;
+                        let gameDate = new Date(gamedetails.data.gameCreation);
+                        gamedetails.data.participantIdentities.forEach(function (game:any){
+                            if(game.player.summonerName == name){
+                                userParticipantId = game.participantId;
+                            }
+                        })
+
+                        gamedetails.data.participants.forEach(function (game:any){
+                            console.log(game.participantId);
+                            console.log(userParticipantId);
+                            if(parseInt(game.participantId) == userParticipantId){
+                                console.log("asd");
+                                let summoners:any[] = [];
+                                summoners.push(game.spell1Id);
+                                summoners.push(game.spell2Id);
+                                let itens:any[] = [];
+                                
+                                let stats = game.stats;
+                                gamesWithDetails.push({
+                                    gameId:record.gameId,
+                                    champion:record.champion,
+                                    queue:record.queue,
+                                    win:stats.win,
+                                    kills:stats.kills,
+                                    deaths:stats.deaths,
+                                    assists:stats.assists,
+                                    summoners:summoners,
+                                    champLevel:stats.champLevel,
+                                    itens:itens,
+                                    duration:duration,
+                                    date:gameDate
+                                });
+                            }
+                        });
+                    }
+                });
+                
+                let queue = await axios.get('http://static.developer.riotgames.com/docs/lol/queues.json');
+                gamesWithDetails.forEach(function (record:any){
+                    console.log("entrei");
+                    queue.data.forEach(function(data:any){
+                        if(queue == data.queueId){
+                            record.queueDetails.push({map:data.map,description:data.description});
+                        }
+                    })
+                });
+
+                rift.data.games = gamesWithDetails;
+            }
+
+            
         }
         return {
             code: 202,
