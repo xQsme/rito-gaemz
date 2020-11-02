021822079550788993d1bc2855acfa09a73e3332
SWarrior97 <32170183+SWarrior97@users.noreply.github.com>
2020-11-02 15:38:43 +0000

    match history

--- react/src/actions/riftProfile.ts
+++ react/src/actions/riftProfile.ts
@@ -4,12 +4,16 @@ import ADDRESS from '../constants/server';
 import {
     START_RIFT_MASTERY_REQUEST,
     RETURN_RIFT_MASTERY,
-    FAIL_RETURN_RIFT_MASTERY
+    FAIL_RETURN_RIFT_MASTERY,
+    START_RIFT_HISTORY_REQUEST,
+    RETURN_RIFT_HISTORY,
+    FAIL_RETURN_RIFT_HISTORY
 } from './types';
 
 import type { RiftMasteryResult } from '../interfaces';
+import type { RiftHistoryResult } from '../interfaces';
 
-const startRequest = ():{type:string} => ({
+const startMasteryRequest = ():{type:string} => ({
     type: START_RIFT_MASTERY_REQUEST,
 });
 
@@ -18,19 +22,49 @@ const returnMastery = (data:RiftMasteryResult):{type:string, payload:RiftMastery
     payload: data,
 });
 
-const failReturn = ():{type:string} => ({
+const failMasteryReturn = ():{type:string} => ({
     type: FAIL_RETURN_RIFT_MASTERY,
+});
+
+const startHistoryRequest = ():{type:string} => ({
+    type: START_RIFT_HISTORY_REQUEST,
+});
+
+const returnHistory = (data:RiftHistoryResult):{type:string, payload:RiftHistoryResult} => ({
+    type: RETURN_RIFT_HISTORY,
+    payload: data,
+});
+
+const failReturnHistory = ():{type:string} => ({
+    type: FAIL_RETURN_RIFT_HISTORY,
 });
 
 export const requestRiftMastery = (region: number, summonerId:string)  => {
     return async (dispatch:(obj:{type:string, payload?: RiftMasteryResult}) => string) => {
         try {
-            dispatch(startRequest());
+            dispatch(startMasteryRequest());
             const result:any = await axios.get(ADDRESS + '/rift/mastery/' + region + '/' + summonerId);
             dispatch(returnMastery(result.data));
             return '';
         } catch (error) {
-            dispatch(failReturn());
+            dispatch(failMasteryReturn());
+            if(error && error.response && error.response.status === 403) {
+                return '❌ API Key Expired!';
+            } else {
+                return '❌ Request Limit Reached!';
+            }
+        }
+    };
+};
+export const requestRiftHistory = (region: number, summonerId:string)  => {
+    return async (dispatch:(obj:{type:string, payload?: RiftHistoryResult}) => string) => {
+        try {
+            dispatch(startHistoryRequest());
+            const result:any = await axios.get(ADDRESS + '/history/' + region + '/' + summonerId);
+            dispatch(returnHistory(result.data));
+            return '';
+        } catch (error) {
+            dispatch(failReturnHistory());
             if(error && error.response && error.response.status === 403) {
                 return '❌ API Key Expired!';
             } else {
--- react/src/components/riftProfile.tsx
+++ react/src/components/riftProfile.tsx
@@ -7,26 +7,43 @@ import { toast } from "react-toastify";
 import type { SearchReducer } from '../interfaces';
 
 import {requestRiftMastery} from '../actions';
+import {requestRiftHistory} from '../actions';
 
 interface RiftProfileProps {
   search:SearchReducer,
   riftProfile:any,
   requestRiftMastery:Function,
+  requestRiftHistory:Function
 }
 
 function RiftProfile(props: RiftProfileProps) {
   const { rift , region} = props.search;
-  const { mastery } = props.riftProfile;
+  const { mastery , history } = props.riftProfile; //rever
 
   //Component Did Mount
   React.useEffect(() => {
 
     rift && !mastery && requestMastery(region, rift.id);
+    rift && !history && requestHistory(region, rift.id);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [rift]);
 
   const requestMastery = async (region: number, summonerId: string) => {
     const result = await requestRiftMastery(region, summonerId);
+    if (result) {
+      toast(result, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
+  const requestHistory = async (region: number, summonerId: string) => {
+    const result = await requestRiftHistory(region, summonerId);
     if (result) {
       toast(result, {
         position: "top-right",
@@ -56,4 +73,8 @@ function mapStateToProps({ search, riftProfile}: any) {
   };
 }
 
-export default connect(mapStateToProps, {requestRiftMastery})(RiftProfile);
+export default connect(mapStateToProps, {
+  requestRiftMastery,
+  requestRiftHistory,
+}
+)(RiftProfile);
