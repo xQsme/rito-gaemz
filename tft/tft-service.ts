e8b3bdf928908f82471fb33ef56caacca87645a6
yvtq8k3n <yvtq8k3n@gmail.com>
2020-11-03 19:51:19 +0000

    Merge branch 'main' of https://github.com/xQsme/rito-gaemz into main sorry if something broke

--- tft/tft-service.ts
+++ tft/tft-service.ts
@@ -105,7 +105,6 @@ async function getUnits(serverNumber: number) {
     }
 }
 
-<<<<<<< HEAD
 
 async function getPlayerStatistics(topChallengers:Object, summonerId:String, server:String, region:String){
     return new Promise<UnitObject>(async (resolve, reject) => {
@@ -237,29 +236,5 @@ async function getProfile(serverNumber: number, summonerId: string) {
         }
     }
 }
-=======
-// async function getProfile(serverNumber: number, summonerId: string) {
-//     const server = getServer(serverNumber);
-//     try{
-//         let response = await axios.get('https://' + server + '/tft/league/v1/entries/by-summoner/' + summonerId + '?api_key=' + key);
-//         return {
-//             code: 202,
-//             data: response.data.length > 0 ? response.data[0] : {tier: 'Unranked'},
-//         }
-//     } catch (error) {
-//         console.log(error);
-//         if(error.response.data.status.status_code === 403) {
-//             return {
-//                 code: 403,
-//                 data: 'Error',
-//             }
-//         }
-//         return {
-//             code: 400,
-//             data: 'Error',
-//         }
-//     }
-// }
->>>>>>> 2eb011d809988367e394d8132eec1ad249487bca
 
 export {};
