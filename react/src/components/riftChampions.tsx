1504ec02281128d11f2663476eea5a8517d27f45
Gandini <Gandini@users.noreply.github.com>
2020-11-03 16:04:43 +0000

    progress rift champions

--- react/src/components/riftChampions.tsx
+++ react/src/components/riftChampions.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from "react-redux";
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Grid, { GridSpacing } from '@material-ui/core/Grid';
+// import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import Grid, {  } from '@material-ui/core/Grid';
 // import FormLabel from '@material-ui/core/FormLabel';
 // import FormControlLabel from '@material-ui/core/FormControlLabel';
 // import RadioGroup from '@material-ui/core/RadioGroup';
@@ -10,35 +10,42 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import ButtonBase from '@material-ui/core/ButtonBase';
 
+// import GridList from '@material-ui/core/GridList';
+// import GridListTile from '@material-ui/core/GridListTile';
+// import GridListTileBar from '@material-ui/core/GridListTileBar';
+// import ListSubheader from '@material-ui/core/ListSubheader';
+// import IconButton from '@material-ui/core/IconButton';
+// import InfoIcon from '@material-ui/icons/Info';
 
 
-import { toast } from "react-toastify";
+// import { toast } from "react-toastify";
 
 import {requestRiftChampions} from '../actions';
+import { blue } from '@material-ui/core/colors';
 
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      height: 140,
-      width: 100,
-    },
-    control: {
-      padding: theme.spacing(2),
-    },
-  }),
-);
+// const useStyles = makeStyles((theme: Theme) =>
+//   createStyles({
+//     root: {
+//       flexGrow: 4,
+//     },
+//     paper: {
+//       height: 140,
+//       width: 100,
+//     },
+//     control: {
+//       padding: theme.spacing(2),
+//     },
+//   }),
+// );
 
 function RiftChampions(props: any) {
-  const [spacing, setSpacing] = React.useState<GridSpacing>(2);
-  const classes = useStyles();
+  // const [spacing, setSpacing] = React.useState<GridSpacing>(2);
+  // const classes = useStyles();
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSpacing(Number((event.target as HTMLInputElement).value) as GridSpacing);
-  };
+  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   setSpacing(Number((event.target as HTMLInputElement).value) as GridSpacing);
+  // };
 
   
   const { champions } = props.riftChampions;
@@ -61,14 +68,19 @@ function RiftChampions(props: any) {
   // ))}
 
   //concat <a data-featherlight={ `string${this.props.data.imageUrl}` }>
+
+  // background-image: url("http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg")
+
   return (
-      <div className={classes.root}>
-        <Paper className={`paper-champ ${classes.paper}`}>
+    <div className="paper-root">
+      <Paper className="paper-rift-champions">
+       
+        <Grid container spacing={2} >
           {Object.keys(champions).map((key) => (
-            <Grid container spacing={2}>
-              <Grid item>
-                <ButtonBase className={classes.image}>
-                  <img className="champ-img" alt="complex" src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champions[key].id}_0.jpg`} />
+            <div className="rift-champion-container" style={{backgroundImage: `url("http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${champions[key].id}_0.jpg")`}}> 
+              <Grid item > 
+                  <ButtonBase className="rift-champion-button" >
+                  <img className="champ-img" alt="complex" src={`http://ddragon.leagueoflegends.com/cdn/10.22.1/img/champion/${champions[key].id}.png`} />
                   {/* <img className={classes.img} alt="complex" src="/static/images/grid/complex.jpg" /> */}
                 </ButtonBase>
               </Grid>
@@ -95,11 +107,78 @@ function RiftChampions(props: any) {
                   <Typography variant="subtitle1">$19.00</Typography>
                 </Grid>
               </Grid>
-            </Grid>
-          ))}
-        </Paper>
-      </div>
-  )
+            </div>
+       
+             ))}
+        </Grid>
+        
+       
+      </Paper>
+    </div>
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // return (
+  //   <div className="champ-list-root">
+  //     <GridList className="champ-list-grid-list">
+  //       <GridListTile key="Subheader" cols={5} style={{ height: 'auto' }}>
+  //         <ListSubheader component="div">December</ListSubheader>
+  //       </GridListTile>
+  //       {Object.keys(champions).map((key) => (
+       
+  //         <GridListTile key={champions[key].key}>
+  //           <img src={`http://ddragon.leagueoflegends.com/cdn/10.22.1/img/champion/${champions[key].id}.png`} alt={champions[key].id} />
+  //           <GridListTileBar
+  //             title={champions[key].id}
+  //             subtitle={<span>Roles: temp</span>}
+  //             actionIcon={
+  //               <IconButton aria-label={`info about ${champions[key].id}`}>
+  //                 <InfoIcon />
+  //               </IconButton>
+  //             }
+  //           />
+  //         </GridListTile>
+  //       ))}
+  //     </GridList>
+  //   </div>
+  // );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
