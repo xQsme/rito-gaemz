dc6cd36acf1a19e5b6dec7edc2c2b4279a56a263
SWarrior97 <32170183+SWarrior97@users.noreply.github.com>
2020-11-05 15:15:53 +0000

    match history done!

--- react/src/components/elements/riftHistory.tsx
+++ react/src/components/elements/riftHistory.tsx
@@ -17,7 +17,6 @@ import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
@@ -25,7 +24,8 @@ import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 const useRowStyles = makeStyles({
   root: {
     '& > *': {
-      borderBottom: 'unset',
+      borderBottom:'unset',
+      borderTop:'unset',
     },
   },
 });
@@ -34,37 +34,41 @@ function formatTime(date:string) {
   return new Date(date).toLocaleDateString('pt-PT');
 }
 function formatKDA(kda:string) {
-  return kda === null ? 'Perfect' : kda;
+  return kda === null ? '👌' : parseFloat(kda).toFixed(2);
+}
+
+function processWin(win:boolean,bgColor:any){
+  return win ? '#1D8348' : '#B03A2E';
 }
 
 function Row(props:any) {
   const { game } = props;
   const [open, setOpen] = React.useState(false);
   const classes = useRowStyles();
+  let bgColor;
+
+  bgColor = processWin(game.win,bgColor);
 
   return (
     <React.Fragment>
-      <TableRow className={classes.root}>
+      <TableRow className={classes.root} style={{ backgroundColor:bgColor}}>
         <TableCell>
           <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
             {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
           </IconButton>
         </TableCell>
         <TableCell component="th" scope="row">
-            <img src={`http://ddragon.leagueoflegends.com/cdn/10.22.1/img/champion/${game.champion}.png`} alt=""/>
+            <img style={{width:"50px", height:"50px"}} src={`http://ddragon.leagueoflegends.com/cdn/10.22.1/img/champion/${game.champion}.png`} alt=""/>
         </TableCell>
-        <TableCell align="right">{game.kills}</TableCell>
-        <TableCell align="right">{game.deaths}</TableCell>
-        <TableCell align="right">{game.assists}</TableCell>
-        <TableCell align="right">{formatTime(game.date)}</TableCell>
+        <TableCell align="center">{game.kills}/{game.deaths}/{game.assists}</TableCell>
+        <TableCell align="center">{formatKDA(game.kda)}</TableCell>
+        <TableCell align="center">{game.cs}</TableCell>
+        <TableCell align="center">{game.queueDetails[0].map}<br/>{game.duration}    {formatTime(game.date)}</TableCell>
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box margin={1}>
-              <Typography variant="h6" gutterBottom component="div">
-                More Details
-              </Typography>
               <Table size="small" aria-label="purchases">
                 <TableHead>
                   <TableRow>
@@ -74,28 +78,15 @@ function Row(props:any) {
                 </TableHead>
                 <TableBody>
                   <TableCell>{game.items.map((item:any) => (
-                    <img src={`http://ddragon.leagueoflegends.com/cdn/10.22.1/img/item/${item}.png`} alt=""/>
+                    <img style={{width:"50px", height:"50px"}} src={`http://ddragon.leagueoflegends.com/cdn/10.22.1/img/item/${item}.png`} alt=""/>
                   ))}</TableCell>
                   <TableCell>{game.summonerDetails.map((summoner:any) => (
-                    <img src={`${summoner.image}`} alt=""/>
+                    <img style={{width:"50px", height:"50px"}} src={`${summoner.image}`} alt=""/>
                   ))}</TableCell>
                 </TableBody>
               </Table>
-              {"\n"}
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Cs</TableCell>
-                    <TableCell>Kda</TableCell>
-                    <TableCell>Duration</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  <TableCell>{game.cs}</TableCell>
-                  <TableCell>{formatKDA(game.kda)}</TableCell>
-                  <TableCell>{game.duration}</TableCell>
-                </TableBody>
-              </Table>
+              <br/>
+              <br/>
             </Box>
           </Collapse>
         </TableCell>
@@ -116,17 +107,17 @@ function RiftHistory(props: RiftHistoryProps) {
 
   
   return (
-    <div className="summoner-container">
+    <div style={{width:"50%",margin:"auto",padding:"10px"}} className="summoner-container">
       <TableContainer component={Paper}>
         <Table aria-label="collapsible table">
           <TableHead>
             <TableRow>
               <TableCell />
-              <TableCell>Champion</TableCell>
-              <TableCell align="right">Kills</TableCell>
-              <TableCell align="right">Death</TableCell>
-              <TableCell align="right">Assist</TableCell>
-              <TableCell align="right">Date</TableCell>
+              <TableCell >Champion</TableCell>
+              <TableCell align="center">Score</TableCell>
+              <TableCell align="center">KDA</TableCell>
+              <TableCell align="center">CS</TableCell>
+              <TableCell align="center">Info</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
