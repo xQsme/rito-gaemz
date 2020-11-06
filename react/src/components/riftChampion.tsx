551bd870f1bf4b6a236c27fd7a152abd10c1cbf7
Gandini <Gandini@users.noreply.github.com>
2020-11-06 19:20:39 +0000

    progress rift champion, css leftovers cleanup

--- react/src/components/riftChampion.tsx
+++ react/src/components/riftChampion.tsx
@@ -1,24 +1,44 @@
 import React from 'react'
+import { toast } from "react-toastify";
 import { connect } from "react-redux";
-import ButtonBase from '@material-ui/core/ButtonBase';
 
 // import { toast } from "react-toastify";
 
 import {requestRiftChampion} from '../actions';
 
 import type { SingleRiftChampion } from "../interfaces";
+import type { RiftChampionReducer } from '../interfaces';
 
 interface ChampionProps {
-  name: string
+  name: string,
+  champion: SingleRiftChampion
+  requestRiftChampion(name: string): Promise<string>
 }
 
-
-type Props = {path: string};
-
-export default function RiftChampion(props: ChampionProps) {
+function RiftChampion(props: ChampionProps) {
   console.log(props);
+  const champion  = props.champion;
+  React.useEffect(() => {
 
+    //rift && mastery.length === 0 && requestMastery(region, rift.id);
+    !champion && requestRiftChampion();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
+  const requestRiftChampion = async () => {
+    const result = await props.requestRiftChampion(props.name);
+    if (result) {
+      toast(result, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  };
 
   // const { profileIconId, name, summonerLevel, riftRankeds } = props.champion;
   
@@ -40,8 +60,22 @@ export default function RiftChampion(props: ChampionProps) {
   // background-image: url("http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg")
 
   return (
-    <div className="summoner-container">
-      <span> ola {props.name} </span>
+    <div className="champion-container">
+      <div className="rift-champion">
+        <span> ola {props.name} </span>
+      </div>
+      
     </div>    
   )
 }
+
+function mapStateToProps({ requestRiftChampion }: any) {
+  return {
+    requestRiftChampion,
+  };
+}
+
+export default connect(mapStateToProps, {
+  requestRiftChampion,
+})(RiftChampion);
+
