232e3333ab207d738f21f2b6d226e64df5895f82
Pedro Ferreira <pedro-ferreira92@hotmail.com>
2020-11-09 11:19:17 +0000

    Update tftHistory.tsx

--- react/src/components/elements/tftHistory.tsx
+++ react/src/components/elements/tftHistory.tsx
@@ -61,7 +61,7 @@ function TFTHistory(props: TFTHistoryProps) {
                   return(
                     <div className="trait-container tooltip" key={trait.name}>
                       <img src={'/tft/traits/bg' + trait.style + '.png'} className="trait-background"/>
-                      <img src={'/tft/traits/' + trait.name.replace('Set4_', '').replace('Boss', 'theboss') + '.svg'} className="trait-icon"/>
+                      <img src={'/tft/traits/' + trait.name.replace('Set4_', '').replace('Boss', 'theboss').toLowerCase() + '.svg'} className="trait-icon"/>
                       <span className="tooltiptext">{trait.name.replace('Set4_', '').replace('Boss', 'The Boss')}</span>
                     </div>
                   )
